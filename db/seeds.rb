a82505e7155df77ec90f2fc7b58529c22cf85faf
Emmanuel Daniels <emmanueldaniels37@gmail.com>
2020-12-20 17:33:20 -0500

    added marion-county-food-pantries-sitesdata

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,35 +15,35 @@ csv = CSV.parse(csv_text, :headers => true, :encoding => "ISO-8859-1")
 
 require "csv"
 
-csv_text = File.read(Rails.root.join("lib", "seeds", "snap.csv"))
-csv_text = File.read(Rails.root.join("lib", "seeds", "state-operated-facilities.csv"))
+csv_text = File.read(Rails.root.join("lib", "seeds", "marion-county-food-pantries-sites.csv"))
+csv_text = File.read(Rails.root.join("lib", "seeds", "marion-county-food-pantries-sites.csv"))
 csv = CSV.parse(csv_text, :headers => true, :encoding => "ISO-8859-1")
 csv.each do |row|
-  puts row.to_hash["Service Type"], ["Provider Name"], ["Address"], ["City"], ["ZipCode"], ["Latitude"], ["Longitude"]
+  puts row.to_hash["SiteSystem_Name"], ["SiteAddressus_SiteAddressus_address_1"], ["SiteAddressus_SiteAddressus_city"], ["SiteAddressus_SiteAddressus_zip"], ["SiteAddressus_SiteAddressus_county"], ["SiteCustom_Phone1"], ["SiteSystem_Websiteurl"], ["SiteSystem_Description"], ["SiteAddressus_SiteAddressus_latitude"], ["SiteAddressus_SiteAddressus_longitude"]
 end
 
 require "csv"
 
-csv_text = File.read(Rails.root.join("lib", "seeds", "snap.csv"))
-csv_text = File.read(Rails.root.join("lib", "seeds", "state-operated-facilities.csv"))
+csv_text = File.read(Rails.root.join("lib", "seeds", "marion-county-food-pantries-sites.csv"))
+csv_text = File.read(Rails.root.join("lib", "seeds", "marion-county-food-pantries-sites.csv"))
 csv = CSV.parse(csv_text, :headers => true, :encoding => "ISO-8859-1")
 csv.each do |row|
   t = Service.new
   t.service_id = row["service_id"]
-  t.name = row["Provider Name"]
-  t.address = row["Address"]
-  t.city = row["City"]
-  t.zipcode = row["ZipCode"]
-  t.service_type = row["Service Type"]
-  # t.county = row["county"]
-  # t.phone = row["phone"]
-  # t.website = row["website"]
+  t.name = row["SiteSystem_Name"]
+  t.address = row["SiteAddressus_SiteAddressus_address_1"]
+  t.city = row["SiteAddressus_SiteAddressus_city"]
+  t.zipcode = row["SiteAddressus_SiteAddressus_zip"]
+  #t.service_type = row["Service Type"]
+  t.county = row["SiteAddressus_SiteAddressus_county"]
+  t.phone = row["SiteCustom_Phone1"]
+  t.website = row["SiteSystem_Websiteurl"]
   # t.email = row["email"]
   # t.languages = row["languages"]
-  # t.description = row["description"]
+  t.description = row["SiteSystem_Description"]
   # t.transportation = row["transportation"]
-  t.latitude = row["Latitude"]
-  t.longitude = row["Longitude"]
+  t.latitude = row["SiteAddressus_SiteAddressus_latitude"]
+  t.longitude = row["SiteAddressus_SiteAddressus_longitude"]
   # t.accessibility_for_disabilities = row["accessibility_for_disabilities"]
   t.save
   # puts "#{t.service_id}, #{t.address} saved"
