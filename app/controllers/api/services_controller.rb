f171202854dcb99834d69602a75a6720a93981a9
Emmanuel Daniels <emmanueldaniels37@gmail.com>
2020-12-20 19:47:40 -0500

    add create action

--- app/controllers/api/services_controller.rb
+++ app/controllers/api/services_controller.rb
@@ -3,4 +3,24 @@ class Api::ServicesController < ApplicationController
     @services = Service.all
     render "index.json.jb"
   end
+
+  def create
+    @service = Service.new(
+    name: params[:name],
+    service_id: params.[:service_id],
+    service_type: params.[:service_type],
+    address: params.[:address],
+    city: params.[:city],
+    zipcode: params.[:zipcode],
+    county: params.[:county],
+    phone: params.[:phone],
+    website: params.[:website],
+    email: params.[:email],
+    description: params.[:description],
+    latitude: params.[:latitude],
+    longitude: params.[:longitude]
+    )
+    @service.save
+    render "show.json.jb"
+  end
 end
